83ee37dc08c24fe81e2dcd7ba5c679f5f658f0b7
Filipe C Menezes <filipe.menezes@mongodb.com>
2024-09-30 17:56:20 +0100

    add rag

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
+import os
+from operator import itemgetter
+
 import streamlit as st
 import ollama
-
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_ollama import ChatOllama
+from langchain_ollama import ChatOllama, OllamaEmbeddings
+from pymongo import MongoClient
+from langchain_mongodb import MongoDBAtlasVectorSearch
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_transformers import MarkdownifyTransformer
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-SYSTEM_MESSAGE = "You're a helpful assistant. Answer all questions to the best of your ability. If you don't know the answer let the user know to find help in the internet."
+SYSTEM_MESSAGE = """You're a helpful assistant. Answer all questions to the best of your ability. If you don't know the answer let the user know to find help in the internet.
 
+Available context:
+{context}
+"""
 MODEL = "llama3.2"
+EMBEDDING_MODEL = "nomic-embed-text"
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
-with st.spinner("Loading"):
-    ollama.pull(MODEL)
+ollama.pull(MODEL)
+ollama.pull(EMBEDDING_MODEL)
+
+mongo_client = MongoClient(MONGO_URI)
+collection = mongo_client["bot"]["data"]
+embedding = OllamaEmbeddings(model=EMBEDDING_MODEL)
+
+if collection.find_one({}) is None:
+    loader = WebBaseLoader("https://en.wikipedia.org/wiki/Llama_(language_model)")
+    md = MarkdownifyTransformer()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200)
+    docs = loader.load()
+    converted_docs = md.transform_documents(docs)
+    splits = text_splitter.split_documents(converted_docs)
+    vectorstore = MongoDBAtlasVectorSearch.from_documents(
+        splits, embedding, collection=collection, index_name="default")
+    vectorstore.create_vector_search_index(768)
+else:
+    vectorstore = MongoDBAtlasVectorSearch.from_connection_string(
+        connection_string=MONGO_URI, namespace="bot.data", embedding=embedding)
+retriever = vectorstore.as_retriever()
 
 chat = ChatOllama(model=MODEL)
 
@@ -24,7 +56,8 @@ prompt_template = ChatPromptTemplate.from_messages(
     ]
 )
 
-chain = prompt_template | chat | StrOutputParser()
+chain = {"context": itemgetter("input") | retriever, "input": itemgetter(
+    "input")} | prompt_template | chat | StrOutputParser()
 
 st.title("Chatbot")
 st.caption("A Streamlit chatbot")
@@ -32,4 +65,4 @@ st.caption("A Streamlit chatbot")
 if prompt := st.chat_input():
     st.chat_message("user").write(prompt)
     with st.chat_message("ai"):
-        st.write_stream(chain.stream(prompt))
+        st.write_stream(chain.stream({"input":prompt}))
